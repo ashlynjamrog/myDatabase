d2323a0f07478b8feb85c9efc2aa95c71f8c6ab9
ajamrog <31996492+ashlynjamrog@users.noreply.github.com>
2022-02-22 21:30:23 -0800

    added tabs

--- App.js
+++ App.js
@@ -6,18 +6,43 @@ import Counter from './Counter/counter.js';
 import FlightBooker from './FlightBooker/flightbooker.js';
 import Timer from './Timer/timer.js';
 import Crud from './CRUD/crud.js';
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Counter></Counter>
-      <Converter></Converter>
-      <FlightBooker></FlightBooker>
-      <Timer></Timer>
-      <Crud></Crud>
-    </View>
-   
+    <Tabs>
+      <TabList>
+        <Tab>Counter</Tab>
+        <Tab>Degree Converter</Tab>
+        <Tab>Flight Booker</Tab>
+        <Tab>Timer</Tab>
+        <Tab>CRUD</Tab>
+      </TabList>
+      <TabPanel>
+        <Counter></Counter>
+      </TabPanel>
+      <TabPanel>
+        <Converter></Converter>
+      </TabPanel>
+      <TabPanel>
+        <FlightBooker></FlightBooker>
+      </TabPanel>
+      <TabPanel>
+        <Timer></Timer>
+      </TabPanel>
+      <TabPanel>
+        <Crud></Crud>
+      </TabPanel>
+    </Tabs>
+    // <View style={styles.container}>
+    //   <Counter></Counter>
+    //   <Converter></Converter>
+    //   <FlightBooker></FlightBooker>
+    //   <Timer></Timer>
+    //   <Crud></Crud>
+    // </View>
+
   );
 }
 
